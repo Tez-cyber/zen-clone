39060bb8406ddd7236361491efa8534a175c3414
Salami Azeez <salamitopeazeez@gmail.com>
2024-12-05 05:28:57 +0100

    Added css styles

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div>hero</div>
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+        <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
+            <div>
+                <div className="mask-clip-path">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
