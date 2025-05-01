3e213a5d558070ace65eb3795b4cb5bf600ef5e1
Salami Azeez <salamitopeazeez@gmail.com>
2025-05-01 13:59:46 +0100

    Added Bento card

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,20 +1,75 @@
+const BentoCard = ({ src, title, desc }) => {
 
+    return (
+        <div className="relative size-full">
+            <video
+                src={src}
+                loop
+                muted
+                autoPlay
+                className="absolute top-0 left-0 size-full object-cover object-center"
+            />
+            <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+                <div>
+                    <h1 className="bento-title special-font">{title}</h1>
+                    {
+                        desc &&
+                        <p className="mt-3 max-w-64 text-xs md:text-base">{desc}</p>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export const Features = () => {
-  return (
-    <section className="bg-black pb-52">
-        <div className="container mx-auto px-3 md:px-10">
-            <div className="px-5 py-32">
-                <p className="font-circular-web text-lg text-blue-50">
-                    Into the Metagame Layer
-                </p>
+    return (
+        <section className="bg-black pb-52">
+            <div className="container mx-auto px-3 md:px-10">
+                <div className="px-5 py-32">
+                    <p className="font-circular-web text-lg text-blue-50">
+                        Into the Metagame Layer
+                    </p>
+                    <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+                        Immerse yourself in a rich  and ever-expanding universe
+                        where a vibrant array of products converge into an interconnected
+                        overlay experience on your world.
+                    </p>
+                </div>
+                <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden
+            rounded-md md:h-[65vh]
+        ">
+                    <BentoCard
+                        src="videos/feature-1.mp4"
+                        title={
+                            <>radiant</>
+                        }
+                        desc={`
+                    A cross-platform metagame app, turning your activities across 
+                    Web2 and Web3 games into a rewarding adventure
+                `}
+                    />
+                </div>
+                <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+                    <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+                        <BentoCard src="videos/feature-2.mp4"
+                            title={<>zigma</>}
+                            desc="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+                        />
+                    </div>
+                    <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+                        <BentoCard
+                            src="videos/feature-3.mp4"
+                            title={
+                                <>
+                                    nexus
+                                </>
+                            }
+                            description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
+                        />
+                    </div>
+                </div>
             </div>
-            <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-                Immerse yourself in a rich  and ever-expanding universe
-                where a vibrant array of products converge into an interconnected 
-                overlay experience on your world.
-            </p>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
